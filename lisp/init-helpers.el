879d09c539121e7da7d04ef9a38742d3e5ad134d
thiagolopes <thiagolopes@protonmail.com>
2021-11-27 15:27:44 -0300

    Update change-state-on-view-mode to use modus theme

--- lisp/init-helpers.el
+++ lisp/init-helpers.el
@@ -3,26 +3,9 @@
 ;;; Code:
 (defun refined/change-state-on-view-mode ()
   (let ((is-active? view-mode)
-	(bg-active (modus-themes-color 'red-refine-bg))
-	(bg-inactive (modus-themes-color 'bg-active)))
-    ;; (setq-local cursor-type (if is-active? 'box 'bar))
+	(bg-active (zenburn-with-color-variables zenburn-red-6))
+	(bg-inactive (zenburn-with-color-variables zenburn-bg-1)))
     (face-remap-add-relative 'mode-line :background (if is-active? bg-active bg-inactive))))
-
-;;; Automatically overriding stale locks
-(defun emacs-process-p (pid)
-  "If pid is the process ID of an emacs process, return t, else nil.
-Also returns nil if pid is nil."
-  (when pid (let* ((cmdline-file (concat "/proc/" (int-to-string pid) "/cmdline")))
-	      (when (file-exists-p cmdline-file)
-		(with-temp-buffer (insert-file-contents-literally cmdline-file)
-				  (goto-char (point-min))
-				  (search-forward "emacs" nil t) pid)))))
-
-(defadvice desktop-owner (after pry-from-cold-dead-hands activate)
-  "Don't allow dead emacsen to own the desktop file."
-  (when (not (emacs-process-p ad-return-value))
-    (setq ad-return-value nil)))
-
 
 (defun refined/toggle-fullscreen ()
   "Toggle full screen"
