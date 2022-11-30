920ddaef57c1392cb10d177bccd63955935d83cc
thiagolopes <thiagolopes@protonmail.com>
2022-11-29 21:30:41 -0300

    Improve modeline package

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -1,10 +1,12 @@
-(defun modeline/buffer-state ()
+;; -*- lexical-binding: t; -*-
+
+(defun buffer-state ()
   (if (buffer-modified-p) " <*MOD*>"
     " <SAVED>"))
 
-(defun modeline/projectile-name-or-none ()
+(defun projectile-name-or-none ()
   (if (projectile-project-name)
-      (format "[%s] " (projectile-project-name))))
+      (format "[%s]" (projectile-project-name))))
 
 (defun dither-xpm (color1 color2)
   "Return an XPM dither string representing."
@@ -32,13 +34,16 @@ static char * dither[] = {
 \"....... . . \",
 \".. . .      \"};" color1 color2))
 
-(defun pl/hex-color (color)
+(defun hex-color (color)
   "Get the hexadecimal value of COLOR."
   (when color
     (let ((srgb-color (color-name-to-rgb color)))
       (apply 'color-rgb-to-hex srgb-color))))
 
-(defun pl/make-xpm (name color1 color2 data)
+(defun total-lines ()
+  (number-to-string (count-lines (point-min) (point-max))))
+
+(defun make-xpm (name color1 color2 data)
   "Return an XPM image with NAME using COLOR1 and COLOR2 bits specified in DATA.
 COLOR1 signifies enabled, and COLOR2 signifies disabled."
   (when window-system
@@ -53,8 +58,8 @@ static char * %s[] = {
 	      (downcase (replace-regexp-in-string " " "_" name))
 	      (length (car data))
 	      (length data)
-	      (or (pl/hex-color color1) "None")
-	      (or (pl/hex-color color2) "None"))
+	      (or (hex-color color1) "None")
+	      (or (hex-color color2) "None"))
       (let ((len (length data))
 	    (idx 0))
 	(apply 'concat
@@ -74,7 +79,7 @@ static char * %s[] = {
 		       data))))
      'xpm t :scale 1 :ascent 'center)))
 
-(defun pl/percent-xpm
+(defun percent-xpm
     (height pmax pmin winend winstart width color1 color2)
   "Generate percentage xpm of HEIGHT for PMAX to PMIN given WINEND and WINSTART.
 Use WIDTH and COLOR1 and COLOR2."
@@ -91,10 +96,10 @@ Use WIDTH and COLOR1 and COLOR2."
 		    (append (make-list width 0)))
 		  data))
       (setq i (+ i 1)))
-    (pl/make-xpm "percent" color1 color2 (reverse data))))
+    (make-xpm "percent" color1 color2 (reverse data))))
 
 
-(defun powerline-hud (&optional color1 color2 width)
+(defun powerline-hud (&optional width)
   "Return XPM of relative buffer location using FACE1 and FACE2 of optional WIDTH."
   (unless width (setq width 3))
   (let ((color2 (face-attribute 'region :background))
@@ -108,28 +113,66 @@ Use WIDTH and COLOR1 and COLOR2."
       (widen)
       (setq pmax (point-max))
       (setq pmin (point-min)))
-    (pl/percent-xpm height pmax pmin we ws
-		    (* (frame-char-width) width) color1 color2)))
+    (percent-xpm height pmax pmin we ws
+		 (* (frame-char-width) width) color1 color2)))
 
 (defun highlight-block ()
   (let ((color1 (face-attribute 'mode-line-inactive :background))
 	(color2 (face-attribute 'font-lock-comment-face :foreground)))
     (list :background color1 :foreground color2 :weight 'bold)))
 
+
+(defun line-length (n)
+  "Length of the Nth line."
+  (save-excursion
+    (goto-char (point-min))
+    (if (zerop (forward-line (1- n)))
+	(- (line-end-position)
+	   (line-beginning-position)))))
+
+(line-length (string-to-number (nth 1 (split-string (what-line)))))
+
 (setq-default mode-line-format
 	      (list
-	       '(:eval (modeline/buffer-state))
-	       " "
+	       '(:eval
+		 (propertize " " 'face
+			     (list
+			      :height 0.3
+			      :background (face-attribute 'success :foreground))))
+	       '(:eval
+		 (propertize " " 'face
+			     (list
+			      :height 0.3
+			      :background (face-attribute 'mode-line-inactive :background))))
+
+
+	       '(:eval
+		 (propertize " " 'face
+			     (list
+			      :height 0.3
+			      :background (face-attribute 'mode-line-inactive :background))))
 	       '(:eval (propertize "%b" 'face (highlight-block)))
-	       " => "
-	       "%f "
-	       '(:eval (propertize (modeline/projectile-name-or-none) 'face '(:foreground "white")))
-	       "-"
+	       '(:eval
+		 (propertize " " 'face
+			     (list
+			      :height 0.3)))
+	       "%f"
+	       'mode-line-modified
+	       '(:eval
+		 (propertize " " 'face
+			     (list
+			      :height 0.3)))
+	       '(:eval (propertize (projectile-name-or-none) 'face '(:foreground "white")))
 	       '(vc-mode vc-mode)
 
-	       '(:eval (propertize " " 'display `(space :align-to (- (+ right right-fringe right-margin) 20))))
-	       '(:eval (propertize " " 'display (powerline-hud)))
-	       "%o "))
+	       '(:eval (propertize " " 'display `(space :align-to (- (+ right right-fringe right-margin) 22))))
 
+	       " %o "
+	       '(line-number-mode "L%l")
+	       ":"
+	       '(:eval (total-lines))
+	       " "
+	       '(:eval (propertize " " 'display (powerline-hud)))
+	       ))
 
 (provide 'init-modeline)
