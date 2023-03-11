0c961053b5cd41e3e14753a277e60be67719446e
thiagolopes <thiagolopes@protonmail.com>
2023-03-11 01:01:33 -0300

    Add ui config in editor

--- core/editor.el
+++ core/editor.el
@@ -60,8 +60,41 @@
       recentf-auto-cleanup 'never)
 (recentf-mode +1)
 
-
 ;; highlight the current line
 (global-hl-line-mode +1)
+
+;; linue number
+(global-display-line-numbers-mode)
+
+;; already disabled anyway
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; disable the annoying bell ring
+(setq ring-bell-function 'ignore)
+
+;; disable startup screen
+(setq inhibit-startup-screen t)
+
+;; nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; more useful frame title, that show either a file or a
+;; buffer name (if the buffer isn't visiting a file)
+(setq frame-title-format
+      '(""(:eval (if (buffer-file-name)
+                                            (abbreviate-file-name (buffer-file-name))
+                                          "%b"))))
+
 
 (provide 'editor)
