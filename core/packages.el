6e8bca4dc24a4ffc893610fa9b099ce1847ddb6d
thiagolopes <thiagolopes@protonmail.com>
2023-03-29 19:03:51 -0300

    Remove diff-hl to git-gutter

--- core/packages.el
+++ core/packages.el
@@ -5,7 +5,6 @@
     ag
     all-the-icons
     browse-kill-ring
-    diff-hl
     diminish
     discover-my-major
     dockerfile-mode
@@ -266,5 +265,16 @@
 (use-package undo-tree
   :init
   (global-undo-tree-mode))
+
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
 
 (provide 'packages)
