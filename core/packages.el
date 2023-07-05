6c3cb402f1f656bde658a3a8aae0050f9e8b4f0f
thiagolopes <thiagolopes@protonmail.com>
2023-07-05 15:09:18 -0300

    Add multiple cursors

--- core/packages.el
+++ core/packages.el
@@ -355,4 +355,10 @@
 
 (use-package tree-sitter-langs)
 
+(use-package multiple-cursors
+  :config
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
 (provide 'packages)
