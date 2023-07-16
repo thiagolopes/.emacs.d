ede37a929f706757b6f05516895b3ff3fd786e64
thiagolopes <thiagolopes@protonmail.com>
2023-07-15 21:22:30 -0300

    Remove Commentaries

--- core/editor.el
+++ core/editor.el
@@ -195,10 +195,8 @@
         (with-current-buffer (get-buffer "*scratch*")
           (delete-region (point-min) (point-max))
           (insert-file-contents scratch-file)))))
-
 (add-hook 'after-init-hook 'load-persistent-scratch)
 (add-hook 'kill-emacs-hook 'save-persistent-scratch)
-
 (run-with-idle-timer 300 t 'save-persistent-scratch)
 
 ;; initial window size
@@ -206,20 +204,11 @@
   (interactive)
   (if window-system
       (progn
-        ;; use 120 char wide window for largeish displays
-        ;; and smaller 80 column windows for smaller displays
-        ;; pick whatever numbers make sense for you
         (if (> (x-display-pixel-width) 1280)
             (add-to-list 'default-frame-alist (cons 'width 120))
           (add-to-list 'default-frame-alist (cons 'width 80)))
-        ;; for the height, subtract a couple hundred pixels
-        ;; from the screen height (for panels, menubars and
-        ;; whatnot), then divide by the height of a char to
-        ;; get the height we want
         (add-to-list 'default-frame-alist
                      (cons 'height (x-display-pixel-height))))))
-
 (set-frame-size-according-to-resolution)
-
 
 (provide 'editor)
