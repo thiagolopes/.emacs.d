c0b9d76e40e3e130378d0c1be678ff11f094d5ec
thiagolopes <thiagolopes@protonmail.com>
2023-09-05 20:59:37 -0300

    Clean better-defaults

--- core/better-defaults.el
+++ core/better-defaults.el
@@ -41,14 +41,7 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
-
 (progn
-  (unless (or (fboundp 'helm-mode) (fboundp 'ivy-mode) (bound-and-true-p fido-vertical-mode))
-    (ido-mode t)
-    (setq ido-enable-flex-matching t))
-
-  (unless (memq window-system '(mac ns))
-    (menu-bar-mode -1))
   (when (fboundp 'tool-bar-mode)
     (tool-bar-mode -1))
   (when (fboundp 'scroll-bar-mode)
@@ -59,39 +52,18 @@
   (autoload 'zap-up-to-char "misc"
     "Kill up to, but not including ARGth occurrence of CHAR." t)
 
-  (require 'uniquify)
-  (setq uniquify-buffer-name-style 'forward)
-
   (defalias 'yes-or-no-p 'y-or-n-p)
-  ;; https://www.emacswiki.org/emacs/SavePlace
-  (save-place-mode 1)
-
-  (global-set-key (kbd "M-/") 'hippie-expand)
-  (global-set-key (kbd "C-x C-b") 'ibuffer)
-  (global-set-key (kbd "M-z") 'zap-up-to-char)
-
-  (global-set-key (kbd "C-s") 'isearch-forward-regexp)
-  (global-set-key (kbd "C-r") 'isearch-backward-regexp)
-  (global-set-key (kbd "C-M-s") 'isearch-forward)
-  (global-set-key (kbd "C-M-r") 'isearch-backward)
 
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
-  (savehist-mode 1)
+
   (setq save-interprogram-paste-before-kill t
         apropos-do-all t
         mouse-yank-at-point t
         require-final-newline t
-        visible-bell t
-        load-prefer-newer t
         backup-by-copying t
         frame-inhibit-implied-resize t
-        ediff-window-setup-function 'ediff-setup-windows-plain
-        custom-file (expand-file-name "custom.el" user-emacs-directory))
-
-  (unless backup-directory-alist
-    (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                                   "backups"))))))
+        ediff-window-setup-function 'ediff-setup-windows-plain)
 
 (provide 'better-defaults)
 ;;; better-defaults.el ends here
