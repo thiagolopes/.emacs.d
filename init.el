74e6e902a8e9de65b2450f70a7376e48dab6b403
thiagolopes <thiagolopes@protonmail.com>
2023-10-06 12:28:48 -0300

    Upgade init with more packages

--- init.el
+++ init.el
@@ -16,6 +16,51 @@
 (custom-set-faces
  '(default ((t ( :weight normal :height 100 :width normal :family "Fira Code")))))
 
+(setq whitespace-line-column nil
+      whitespace-style
+      '(face indentation tabs tab-mark spaces space-mark newline newline-mark
+             trailing lines-tail)
+      whitespace-display-mappings
+      '((tab-mark ?\t [?› ?\t])
+        (newline-mark ?\n [?¬ ?\n])
+        (space-mark ?\  [?·] [?.])))
+
+(setq dired-dwim-target t  ; suggest a target for moving/copying intelligently
+      dired-hide-details-hide-symlink-targets nil
+      ;; don't prompt to revert, just do it
+      dired-auto-revert-buffer #'dired-buffer-stale-p
+      ;; Always copy/delete recursively
+      dired-recursive-copies  'always
+      dired-recursive-deletes 'top
+      ;; Ask whether destination dirs should get created when copying/removing files.
+      dired-create-destination-dirs 'ask
+      ;; Where to store image caches
+      image-dired-dir (concat user-emacs-directory "image-dired/")
+      image-dired-db-file (concat image-dired-dir "db.el")
+      image-dired-gallery-dir (concat image-dired-dir "gallery/")
+      image-dired-temp-image-file (concat image-dired-dir "temp-image")
+      image-dired-temp-rotate-image-file (concat image-dired-dir "temp-rotate-image")
+      ;; Screens are larger nowadays, we can afford slightly larger thumbnails
+      image-dired-thumb-size 150)
+
+(setq enable-recursive-minibuffers t)
+;; Show current key-sequence in minibuffer ala 'set showcmd' in vim. Any
+;; feedback after typing is better UX than no feedback at all.
+(setq echo-keystrokes 0.02)
+;; Expand the minibuffer to fit multi-line text displayed in the echo-area. This
+;; doesn't look too great with direnv, however...
+(setq resize-mini-windows 'grow-only)
+;; Typing yes/no is obnoxious when y/n will do
+(if (boundp 'use-short-answers)
+    (setq use-short-answers t)
+  ;; DEPRECATED: Remove when we drop 27.x support
+  (advice-add #'yes-or-no-p :override #'y-or-n-p))
+;; Try to keep the cursor out of the read-only portions of the minibuffer.
+(setq minibuffer-prompt-properties '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+(setq x-select-enable-clipboard-manager nil)
+(setq warning-minimum-level :emergency)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (horizontal-scroll-bar-mode -1)
@@ -24,12 +69,12 @@
 (show-paren-mode 1)
 (setq-default indent-tabs-mode nil)
 (setq save-interprogram-paste-before-kill t
-        apropos-do-all t
-        mouse-yank-at-point t
-        require-final-newline t
-        backup-by-copying t
-        frame-inhibit-implied-resize t
-        ediff-window-setup-function 'ediff-setup-windows-plain)
+      apropos-do-all t
+      mouse-yank-at-point t
+      require-final-newline t
+      backup-by-copying t
+      frame-inhibit-implied-resize t
+      ediff-window-setup-function 'ediff-setup-windows-plain)
 
 ;; A simple frame title
 (setq frame-title-format '("%b – Doom Emacs")
@@ -113,27 +158,15 @@
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'text-mode-hook #'display-line-numbers-mode)
 (add-hook 'conf-mode-hook #'display-line-numbers-mode)
-          
+
+(electric-pair-mode t)
+
 (setq display-line-numbers-type t
       org-directory "~/org/"
       cache-dir (concat user-emacs-directory "/cache")
       custom-file (concat user-emacs-directory "/custom.el")
       require-final-newline t
       load-prefer-newer t)
-
-(use-package expand-region
-  :bind ("M-@" . er/expand-region))
-
-(use-package counsel
-  :commands (counsel-yank-pop counsel-ag counsel-fzf)
-  :bind
-  ("M-?" . counsel-ag)
-  ("C-M-?" . counsel-fzf))
-
-(use-package smartparens
-  :bind
-  ("C-)" . sp-forward-slurp-sexp)
-  ("C-(" . sp-forward-barf-sexp))
 
 (defun map! (&rest key-func-pairs)
   (while key-func-pairs
@@ -164,7 +197,20 @@
 (map! "C-s" #'swiper-isearch-thing-at-point
       "C-S" #'swiper-isearch)
 
-(pixel-scroll-precision-mode 0)
+(use-package diminish)
+(use-package expand-region
+  :bind ("M-@" . er/expand-region))
+
+(use-package counsel
+  :commands (counsel-yank-pop counsel-ag counsel-fzf)
+  :bind
+  ("M-?" . counsel-ag)
+  ("C-M-?" . counsel-fzf))
+
+(use-package smartparens
+  :bind
+  ("C-)" . sp-forward-slurp-sexp)
+  ("C-(" . sp-forward-barf-sexp))
 
 (use-package dashboard
   :config
@@ -173,6 +219,7 @@
 (use-package cider)
 
 (use-package company
+  :diminish
   :hook
   (prog-mode . global-company-mode))
 
@@ -199,23 +246,11 @@
   :config
   (setq hl-todo-highlight-punctuation ":"
         hl-todo-keyword-faces
-        '(;; For reminders to change or add something at a later date.
-          ("TODO" warning bold)
-          ;; For code (or code paths) that are broken, unimplemented, or slow,
-          ;; and may become bigger problems later.
+        '(("TODO" warning bold)
           ("FIXME" error bold)
-          ;; For code that needs to be revisited later, either to upstream it,
-          ;; improve it, or address non-critical issues.
           ("REVIEW" font-lock-keyword-face bold)
-          ;; For code smells where questionable practices are used
-          ;; intentionally, and/or is likely to break in a future update.
           ("HACK" font-lock-constant-face bold)
-          ;; For sections of code that just gotta go, and will be gone soon.
-          ;; Specifically, this means the code is deprecated, not necessarily
-          ;; the feature it enables.
           ("DEPRECATED" font-lock-doc-face bold)
-          ;; Extra keywords commonly found in the wild, whose meaning may vary
-          ;; from project to project.
           ("NOTE" success bold)
           ("BUG" error bold)
           ("XXX" font-lock-constant-face bold))))
@@ -236,7 +271,7 @@
         ido-create-new-buffer 'always
         ido-enable-flex-matching t
         ido-everywhere t))
-  
+
 (use-package ido-vertical-mode
   :hook (ido-mode . ido-vertical-mode)
   :config (setq ido-vertical-show-count t))
@@ -244,39 +279,39 @@
 (use-package ido-sort-mtime
   :hook (ido-mode . ido-sort-mtime-mode))
 
-
 (use-package git-gutter
+  :diminish
   :commands git-gutter:revert-hunk git-gutter:stage-hunk git-gutter:previous-hunk git-gutter:next-hunk
   :init
   (add-hook 'find-file-hook
-    (defun +vc-gutter-init-maybe-h ()
-      "Enable `git-gutter-mode' in the current buffer.
+            (defun +vc-gutter-init-maybe-h ()
+              "Enable `git-gutter-mode' in the current buffer.
 If the buffer doesn't represent an existing file, `git-gutter-mode's activation
 is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
-      (let ((file-name (buffer-file-name (buffer-base-buffer))))
-        (cond
-         ((and (file-remote-p (or file-name default-directory))
-               (not +vc-gutter-in-remote-files)))
-         ;; UX: If not a valid file, wait until it is written/saved to activate
-         ;;   git-gutter.
-         ((not (and file-name (vc-backend file-name)))
-          (add-hook 'after-save-hook #'+vc-gutter-init-maybe-h nil 'local))
-         ;; UX: Allow git-gutter or git-gutter-fringe to activate based on the
-         ;;   type of frame we're in. This allows git-gutter to work for silly
-         ;;   geese who open both tty and gui frames from the daemon.
-         ((if (and (display-graphic-p)
-                   (require 'git-gutter-fringe nil t))
-              (setq-local git-gutter:init-function      #'git-gutter-fr:init
-                          git-gutter:view-diff-function #'git-gutter-fr:view-diff-infos
-                          git-gutter:clear-function     #'git-gutter-fr:clear
-                          git-gutter:window-width -1)
-            (setq-local git-gutter:init-function      'nil
-                        git-gutter:view-diff-function #'git-gutter:view-diff-infos
-                        git-gutter:clear-function     #'git-gutter:clear-diff-infos
-                        git-gutter:window-width 1))
-          (unless (memq major-mode git-gutter:disabled-modes)
-            (git-gutter-mode +1)
-            (remove-hook 'after-save-hook #'+vc-gutter-init-maybe-h 'local)))))))
+              (let ((file-name (buffer-file-name (buffer-base-buffer))))
+                (cond
+                 ((and (file-remote-p (or file-name default-directory))
+                       (not +vc-gutter-in-remote-files)))
+                 ;; UX: If not a valid file, wait until it is written/saved to activate
+                 ;;   git-gutter.
+                 ((not (and file-name (vc-backend file-name)))
+                  (add-hook 'after-save-hook #'+vc-gutter-init-maybe-h nil 'local))
+                 ;; UX: Allow git-gutter or git-gutter-fringe to activate based on the
+                 ;;   type of frame we're in. This allows git-gutter to work for silly
+                 ;;   geese who open both tty and gui frames from the daemon.
+                 ((if (and (display-graphic-p)
+                           (require 'git-gutter-fringe nil t))
+                      (setq-local git-gutter:init-function      #'git-gutter-fr:init
+                                  git-gutter:view-diff-function #'git-gutter-fr:view-diff-infos
+                                  git-gutter:clear-function     #'git-gutter-fr:clear
+                                  git-gutter:window-width -1)
+                    (setq-local git-gutter:init-function      'nil
+                                git-gutter:view-diff-function #'git-gutter:view-diff-infos
+                                git-gutter:clear-function     #'git-gutter:clear-diff-infos
+                                git-gutter:window-width 1))
+                  (unless (memq major-mode git-gutter:disabled-modes)
+                    (git-gutter-mode +1)
+                    (remove-hook 'after-save-hook #'+vc-gutter-init-maybe-h 'local)))))))
 
   ;; UX: Disable in Org mode, as per syl20bnr/spacemacs#10555 and
   ;;   syohex/emacs-git-gutter#24. Apparently, the mode-enabling function for
@@ -285,8 +320,6 @@ is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
   ;;   why this is the case, but adding `fundamental-mode' here fixes the issue.
   (setq git-gutter:disabled-modes '(fundamental-mode image-mode pdf-view-mode))
   :config
-  (set-popup-rule "^\\*git-gutter" :select nil :size '+popup-shrink-to-fit)
-
   ;; PERF: Only enable the backends that are available, so it doesn't have to
   ;;   check when opening each buffer.
   (setq git-gutter:handled-backends
@@ -301,16 +334,6 @@ is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
   (advice-remove #'quit-window #'git-gutter:quit-window)
   (advice-remove #'switch-to-buffer #'git-gutter:switch-to-buffer)
 
-  (add-hook '(doom-escape-hook doom-switch-window-hook) :append
-    (defun +vc-gutter-update-h (&rest _)
-      "Refresh git-gutter on ESC. Return nil to prevent shadowing other
-`doom-escape-hook' hooks."
-      (ignore (or (memq this-command '(git-gutter:stage-hunk
-                                       git-gutter:revert-hunk))
-                  inhibit-redisplay
-                  (if git-gutter-mode
-                      (git-gutter)
-                    (+vc-gutter-init-maybe-h))))))
   ;; UX: update git-gutter when using magit commands
   (advice-add #'magit-stage-file   :after #'+vc-gutter-update-h)
   (advice-add #'magit-unstage-file :after #'+vc-gutter-update-h)
@@ -353,29 +376,11 @@ is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
 
 (use-package amx
   :init
-  (amx-mode 1)
-  
-(setq enable-recursive-minibuffers t)
-;; Show current key-sequence in minibuffer ala 'set showcmd' in vim. Any
-;; feedback after typing is better UX than no feedback at all.
-(setq echo-keystrokes 0.02)
-;; Expand the minibuffer to fit multi-line text displayed in the echo-area. This
-;; doesn't look too great with direnv, however...
-(setq resize-mini-windows 'grow-only)
-;; Typing yes/no is obnoxious when y/n will do
-(if (boundp 'use-short-answers)
-    (setq use-short-answers t)
-  ;; DEPRECATED: Remove when we drop 27.x support
-  (advice-add #'yes-or-no-p :override #'y-or-n-p))
-;; Try to keep the cursor out of the read-only portions of the minibuffer.
-(setq minibuffer-prompt-properties '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode))
+  (amx-mode 1))
 
 (use-package solaire-mode
   :hook (prog-mode-hook . solaire-global-mode)
   :hook (+popup-buffer-mode . turn-on-solaire-mode))
-
-(setq x-select-enable-clipboard-manager nil)
 
 (use-package hl-line
   ;; Highlights the current line
@@ -383,7 +388,7 @@ is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
   :init
   (defvar global-hl-line-modes
     '(prog-mode text-mode conf-mode special-mode
-      org-agenda-mode dired-mode)
+                org-agenda-mode dired-mode)
     "What modes to enable `hl-line-mode' in.")
   :config
   ;; HACK I reimplement `global-hl-line-mode' so we can white/blacklist modes in
@@ -418,16 +423,6 @@ is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
         show-paren-when-point-inside-paren t
         show-paren-when-point-in-periphery t))
 
-;;;###package whitespace
-(setq whitespace-line-column nil
-      whitespace-style
-      '(face indentation tabs tab-mark spaces space-mark newline newline-mark
-        trailing lines-tail)
-      whitespace-display-mappings
-      '((tab-mark ?\t [?› ?\t])
-        (newline-mark ?\n [?¬ ?\n])
-        (space-mark ?\  [?·] [?.])))
-
 (use-package nerd-icons
   :commands (nerd-icons-octicon
              nerd-icons-faicon
@@ -451,6 +446,70 @@ is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
   :config
   (setq rainbow-delimiters-max-face-count 4))
 
-(setq warning-minimum-level :emergency)
+(use-package undo-fu
+  :config
+  ;; Increase undo history limits to reduce likelihood of data loss
+  (setq undo-limit 400000           ; 400kb (default is 160kb)
+        undo-strong-limit 3000000   ; 3mb   (default is 240kb)
+        undo-outer-limit 48000000)  ; 48mb  (default is 24mb)
+  (define-minor-mode undo-fu-mode
+    "Enables `undo-fu' for the current session."
+    :keymap (let ((map (make-sparse-keymap)))
+              (define-key map [remap undo] #'undo-fu-only-undo)
+              (define-key map [remap redo] #'undo-fu-only-redo)
+              (define-key map (kbd "C-_")     #'undo-fu-only-undo)
+              (define-key map (kbd "M-_")     #'undo-fu-only-redo)
+              (define-key map (kbd "C-M-_")   #'undo-fu-only-redo-all)
+              (define-key map (kbd "C-x r u") #'undo-fu-session-save)
+              (define-key map (kbd "C-x r U") #'undo-fu-session-recover)
+              map)
+    :init-value nil
+    :global t)
+  (undo-fu-mode))
+
+(use-package undo-fu-session
+  :hook (undo-fu-mode . global-undo-fu-session-mode)
+  :custom (undo-fu-session-directory (concat user-emacs-directory "undo-fu-session/"))
+  :config
+  (setq undo-fu-session-incompatible-files '("\\.gpg$" "/COMMIT_EDITMSG\\'" "/git-rebase-todo\\'"))
+  (when (executable-find "zstd")
+    ;; There are other algorithms available, but zstd is the fastest, and speed
+    ;; is our priority within Emacs
+    (setq undo-fu-session-compression 'zst))
+  (defadvice! +undo-fu-make-hashed-session-file-name-a (file)
+              :override #'undo-fu-session--make-file-name
+              (concat (let ((backup-directory-alist `(("." . ,undo-fu-session-directory))))
+                        (make-backup-file-name-1 file))
+                      (undo-fu-session--file-name-ext))))
+
+(use-package undo-tree
+  :hook (undo-fu-mode . global-undo-tree-mode)
+  :custom (undo-tree-history-directory-alist `(("." . ,(concat user-emacs-directory "undo-tree-hist/"))))
+  :config
+  (setq undo-tree-visualizer-diff t
+        undo-tree-auto-save-history t
+        undo-tree-enable-undo-in-region t
+        ;; Increase undo limits to avoid emacs prematurely truncating the undo
+        ;; history and corrupting the tree. This is larger than the undo-fu
+        ;; defaults because undo-tree trees consume exponentially more space,
+        ;; and then some when `undo-tree-enable-undo-in-region' is involved. See
+        ;; syl20bnr/spacemacs#12110
+        undo-limit 800000            ; 800kb (default is 160kb)
+        undo-strong-limit 12000000   ; 12mb  (default is 240kb)
+        undo-outer-limit 128000000)) ; 128mb (default is 24mb))
+
+(use-package ibuffer-projectile
+  ;; Group ibuffer's list by project root
+  :hook (ibuffer . ibuffer-projectile-set-filter-groups)
+  :config
+  (setq ibuffer-projectile-prefix
+        (if (modulep! +icons)
+            (concat (nerd-icons-octicon
+                     "nf-oct-file_directory"
+                     :face ibuffer-filter-group-name-face
+                     :v-adjust -0.05)
+                    " ")
+          "Project: ")))
+
 ;; Yes, I really want to quit.
 (setq confirm-kill-emacs nil)
