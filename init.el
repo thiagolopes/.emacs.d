cf3963c83e62b32db0c420dbecb0c7ae2a862ff0
thiagolopes <thiagolopes@protonmail.com>
2023-10-06 15:32:31 -0300

    Split init to early-init

--- init.el
+++ init.el
@@ -1,202 +1,3 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure 't)
-
-(setq user-full-name "Thiago Lopes"
-      user-mail-address "thiagolopes@protonmail.com")
-
-(setq gc-cons-threshold 100000000
-      read-process-output-max (* 1024 1024))
-
-(custom-set-faces
- '(default ((t ( :weight normal :height 100 :width normal :family "Fira Code")))))
-
-(setq whitespace-line-column nil
-      whitespace-style
-      '(face indentation tabs tab-mark spaces space-mark newline newline-mark
-             trailing lines-tail)
-      whitespace-display-mappings
-      '((tab-mark ?\t [?› ?\t])
-        (newline-mark ?\n [?¬ ?\n])
-        (space-mark ?\  [?·] [?.])))
-
-(setq dired-dwim-target t  ; suggest a target for moving/copying intelligently
-      dired-hide-details-hide-symlink-targets nil
-      ;; don't prompt to revert, just do it
-      dired-auto-revert-buffer #'dired-buffer-stale-p
-      ;; Always copy/delete recursively
-      dired-recursive-copies  'always
-      dired-recursive-deletes 'top
-      ;; Ask whether destination dirs should get created when copying/removing files.
-      dired-create-destination-dirs 'ask
-      ;; Where to store image caches
-      image-dired-dir (concat user-emacs-directory "image-dired/")
-      image-dired-db-file (concat image-dired-dir "db.el")
-      image-dired-gallery-dir (concat image-dired-dir "gallery/")
-      image-dired-temp-image-file (concat image-dired-dir "temp-image")
-      image-dired-temp-rotate-image-file (concat image-dired-dir "temp-rotate-image")
-      ;; Screens are larger nowadays, we can afford slightly larger thumbnails
-      image-dired-thumb-size 150)
-
-(setq enable-recursive-minibuffers t)
-;; Show current key-sequence in minibuffer ala 'set showcmd' in vim. Any
-;; feedback after typing is better UX than no feedback at all.
-(setq echo-keystrokes 0.02)
-;; Expand the minibuffer to fit multi-line text displayed in the echo-area. This
-;; doesn't look too great with direnv, however...
-(setq resize-mini-windows 'grow-only)
-;; Typing yes/no is obnoxious when y/n will do
-(if (boundp 'use-short-answers)
-    (setq use-short-answers t)
-  ;; DEPRECATED: Remove when we drop 27.x support
-  (advice-add #'yes-or-no-p :override #'y-or-n-p))
-;; Try to keep the cursor out of the read-only portions of the minibuffer.
-(setq minibuffer-prompt-properties '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-(setq x-select-enable-clipboard-manager nil)
-(setq warning-minimum-level :emergency)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(horizontal-scroll-bar-mode -1)
-(menu-bar-mode -1)
-(defalias 'yes-or-no-p 'y-or-n-p)
-(show-paren-mode 1)
-(setq-default indent-tabs-mode nil)
-(setq save-interprogram-paste-before-kill t
-      apropos-do-all t
-      mouse-yank-at-point t
-      require-final-newline t
-      backup-by-copying t
-      frame-inhibit-implied-resize t
-      ediff-window-setup-function 'ediff-setup-windows-plain)
-
-;; A simple frame title
-(setq frame-title-format '("%b – Doom Emacs")
-      icon-title-format frame-title-format)
-
-;; Don't resize the frames in steps; it looks weird, especially in tiling window
-;; managers, where it can leave unseemly gaps.
-(setq frame-resize-pixelwise t)
-
-;; But do not resize windows pixelwise, this can cause crashes in some cases
-;; when resizing too many windows at once or rapidly.
-(setq window-resize-pixelwise nil)
-
-;; UX: GUIs are inconsistent across systems, desktop environments, and themes,
-;;   and don't match the look of Emacs. They also impose inconsistent shortcut
-;;   key paradigms. I'd rather Emacs be responsible for prompting.
-(setq use-dialog-box nil)
-(when (bound-and-true-p tooltip-mode)
-  (tooltip-mode -1))
-
-;; UX: Favor vertical splits over horizontal ones. Monitors are trending toward
-;;   wide, rather than tall.
-(setq split-width-threshold 160
-      split-height-threshold nil)
-
-;; FIX: The native border "consumes" a pixel of the fringe on righter-most
-;;   splits, `window-divider' does not. Available since Emacs 25.1.
-(setq window-divider-default-places t
-      window-divider-default-bottom-width 1
-      window-divider-default-right-width 1)
-(add-hook 'prog-mode #'window-divider-mode)
-
-;; Don't prompt for confirmation when we create a new file or buffer (assume the
-;; user knows what they're doing).
-(setq confirm-nonexistent-file-or-buffer nil)
-(setq uniquify-buffer-name-style 'forward
-      ;; no beeping or blinking please
-      ring-bell-function #'ignore
-      visible-bell nil)
-
-;; middle-click paste at point, not at click
-(setq mouse-yank-at-point t)
-
-(setq hscroll-margin 2
-      hscroll-step 1
-      ;; Emacs spends too much effort recentering the screen if you scroll the
-      ;; cursor more than N lines past window edges (where N is the settings of
-      ;; `scroll-conservatively'). This is especially slow in larger files
-      ;; during large-scale scrolling commands. If kept over 100, the window is
-      ;; never automatically recentered. The default (0) triggers this too
-      ;; aggressively, so I've set it to 10 to recenter if scrolling too far
-      ;; off-screen.
-      scroll-conservatively 10
-      scroll-margin 0
-      scroll-preserve-screen-position t
-      ;; Reduce cursor lag by a tiny bit by not auto-adjusting `window-vscroll'
-      ;; for tall lines.
-      auto-window-vscroll nil
-      ;; mouse
-      mouse-wheel-scroll-amount '(2 ((shift) . hscroll))
-      mouse-wheel-scroll-amount-horizontal 2)
-
-(blink-cursor-mode -1)
-(setq blink-matching-paren nil)
-(setq x-stretch-cursor nil)
-;; useful information, like git-gutter and flycheck.
-(setq indicate-buffer-boundaries nil
-      indicate-empty-lines nil)
-
-(use-package doom-themes)
-(load-theme 'doom-solarized-dark-high-contrast t)
-
-;; Explicitly define a width to reduce the cost of on-the-fly computation
-(setq-default display-line-numbers-width 3)
-;; Show absolute line numbers for narrowed regions to make it easier to tell the
-;; buffer is narrowed, and where you are, exactly.
-(setq-default display-line-numbers-widen t)
-;; Enable line numbers in most text-editing modes. We avoid
-;; `global-display-line-numbers-mode' because there are many special and
-;; temporary modes where we don't need/want them.
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
-(add-hook 'text-mode-hook #'display-line-numbers-mode)
-(add-hook 'conf-mode-hook #'display-line-numbers-mode)
-
-(electric-pair-mode t)
-
-(setq display-line-numbers-type t
-      org-directory "~/org/"
-      cache-dir (concat user-emacs-directory "/cache")
-      custom-file (concat user-emacs-directory "/custom.el")
-      require-final-newline t
-      load-prefer-newer t)
-
-(defun map! (&rest key-func-pairs)
-  (while key-func-pairs
-    (let ((key (pop key-func-pairs))
-          (func (pop key-func-pairs)))
-      (global-set-key (kbd key) func))))
-
-(map! "C-=" #'text-scale-increase
-      "C-+" #'text-scale-increase
-      "C--" #'text-scale-decrease
-      "M-n" #'forward-paragraph
-      "M-p" #'backward-paragraph)
-
-(map! "<f3>" #'kmacro-start-macro-or-insert-counter
-      "<f4>" #'kmacro-end-or-call-macro)
-
-(map! "M-3" #'(lambda () (interactive) (insert "#"))
-      "M-9" #'(lambda () (interactive) (insert "("))
-      "M-0" #'(lambda () (interactive) (insert ")"))
-      "M-[" #'(lambda () (interactive) (insert "{"))
-      "M-]" #'(lambda () (interactive) (insert "}")))
-
-(map! "C-x C-b" #'ibuffer
-      "C-r" #'isearch-backward-regexp
-      "C-M-s" #'isearch-forward
-      "C-M-r" #'isearch-backward)
-
-(map! "C-s" #'swiper-isearch-thing-at-point
-      "C-S" #'swiper-isearch)
-
 (use-package diminish)
 (use-package expand-region
   :bind ("M-@" . er/expand-region))
@@ -510,6 +311,26 @@ is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
                      :v-adjust -0.05)
                     " ")
           "Project: ")))
+
+(use-package magit
+  :init
+  (setq magit-auto-revert-mode nil)  ; we do this ourselves further down
+  ;; Must be set early to prevent ~/.config/emacs/transient from being created
+  (let ((data-dir user-emacs-directory))
+    (setq transient-levels-file  (concat data-dir "transient/levels")
+          transient-values-file  (concat data-dir "transient/values")
+          transient-history-file (concat data-dir "transient/history")))
+  :config
+  (setq transient-default-level 5
+        magit-diff-refine-hunk t ; show granular diffs in selected hunk
+        ;; Don't autosave repo buffers. This is too magical, and saving can
+        ;; trigger a bunch of unwanted side-effects, like save hooks and
+        ;; formatters. Trust the user to know what they're doing.
+        magit-save-repository-buffers nil
+        ;; Don't display parent/related refs in commit buffers; they are rarely
+        ;; helpful and only add to runtime costs.
+        magit-revision-insert-related-refs nil)
+  (add-hook 'magit-process-mode-hook #'goto-address-mode))
 
 ;; Yes, I really want to quit.
 (setq confirm-kill-emacs nil)
