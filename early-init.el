6319cbff16b242c7036a637b5d73ab84f9fb3f64
thiagolopes <thiagolopes@protonmail.com>
2023-10-07 00:19:34 -0300

    Updating use-package setup

--- early-init.el
+++ early-init.el
@@ -1,11 +1,14 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure 't)
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
 
 (setq user-full-name "Thiago Lopes"
       user-mail-address "thiagolopes@protonmail.com")
