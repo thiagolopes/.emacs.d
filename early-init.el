c8407c4cee53fd4b2391d5c21d5f14aa4c8f7dd9
thiagolopes <thiagolopes@protonmail.com>
2023-11-11 16:29:57 -0300

    Remove blank lines

--- early-init.el
+++ early-init.el
@@ -287,17 +287,14 @@
   (if mwheel-coalesce-scroll-events
       (apply orig args)
     (setq mwheel-coalesce-scroll-events t)))
-
 (defun filter-mwheel-never-coalesce (orig &rest args)
   "A filter function suitable for :around advices that ensures only
    non-coalesced scroll events reach the advised function."
   (if mwheel-coalesce-scroll-events
       (setq mwheel-coalesce-scroll-events nil)
     (apply orig args)))
-
 ; Don't coalesce for high precision scrolling
 (advice-add 'pixel-scroll-precision :around #'filter-mwheel-never-coalesce)
-
 ; Coalesce for default scrolling (which is still used for horizontal scrolling)
 ; and text scaling (bound to ctrl + mouse wheel by default).
 (advice-add 'mwheel-scroll          :around #'filter-mwheel-always-coalesce)
