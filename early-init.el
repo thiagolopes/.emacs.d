7f55572d54ca34541de062c0d182422de685cd46
thiagolopes <thiagolopes@protonmail.com>
2023-11-11 16:27:13 -0300

    Better scroll

--- early-init.el
+++ early-init.el
@@ -279,3 +279,26 @@
 (add-hook 'after-init-hook 'load-persistent-scratch)
 (add-hook 'kill-emacs-hook 'save-persistent-scratch)
 (run-with-idle-timer 300 t 'save-persistent-scratch)
+
+;; better scroll
+ (defun filter-mwheel-always-coalesce (orig &rest args)
+  "A filter function suitable for :around advices that ensures only
+   coalesced scroll events reach the advised function."
+  (if mwheel-coalesce-scroll-events
+      (apply orig args)
+    (setq mwheel-coalesce-scroll-events t)))
+
+(defun filter-mwheel-never-coalesce (orig &rest args)
+  "A filter function suitable for :around advices that ensures only
+   non-coalesced scroll events reach the advised function."
+  (if mwheel-coalesce-scroll-events
+      (setq mwheel-coalesce-scroll-events nil)
+    (apply orig args)))
+
+; Don't coalesce for high precision scrolling
+(advice-add 'pixel-scroll-precision :around #'filter-mwheel-never-coalesce)
+
+; Coalesce for default scrolling (which is still used for horizontal scrolling)
+; and text scaling (bound to ctrl + mouse wheel by default).
+(advice-add 'mwheel-scroll          :around #'filter-mwheel-always-coalesce)
+(advice-add 'mouse-wheel-text-scale :around #'filter-mwheel-always-coalesce)
