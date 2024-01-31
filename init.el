5a0550eeea519d5ba1e6681e63c7709eb0c2b01e
thiagolopes <thiagolopes@protonmail.com>
2024-01-31 10:27:18 -0300

    Add dired-sidebar

--- init.el
+++ init.el
@@ -529,6 +529,27 @@
   :config
   (global-set-key (kbd "M-o") 'ace-window))
 
+(use-package all-the-icons
+  :defer 3
+  :if (display-graphic-p))
+
+(use-package dired-sidebar
+  :defer 2
+  :bind (("<f9>" . dired-sidebar-toggle-sidebar))
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'icons)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
+
 ;; Fringe options
 (fringe-mode)
 (general-setq-default indicate-empty-lines t
