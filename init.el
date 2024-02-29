58574d6b05643f2df5d00142c36f216e45b506f8
thiagolopes <thiagolopes@protonmail.com>
2024-02-29 12:37:57 -0300

    Add ws-butler settings

--- init.el
+++ init.el
@@ -318,6 +318,8 @@
   (add-hook 'dumb-jump-after-jump-hook #'better-jumper-set-jump))
 
 (use-package ws-butler
+  :custom
+  (ws-butler-keep-whitespace-before-point t)
   :config
   (add-hook 'prog-mode-hook #'ws-butler-mode))
 
