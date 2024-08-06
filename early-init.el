ee516039c81fd7692d36ab75f4992d9e25bb30e5
thiagolopes <thiagolopes@protonmail.com>
2024-08-06 17:38:04 -0300

    Configure GC after-init

--- early-init.el
+++ early-init.el
@@ -1,9 +1,10 @@
 ;; GC conf
-(setopt gc-cons-threshold 100000000)
+(setopt gc-cons-threshold (* (* 1024 1024) 100)) ;; 100MB
 (setopt read-process-output-max (* 1024 1024))
 (setopt comp-deferred-compilation t)
 (defvar comp-deferred-compliation)
 (add-hook 'after-init-hook #'garbage-collect t)
+(add-hook 'after-init-hook (lambda () (setopt gc-cons-threshold (* (* 1024 1024) 20)))) ;; back to 20MB
 
 ;; please, maximize
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
