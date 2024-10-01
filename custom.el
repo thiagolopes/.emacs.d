4afae00f6125b720b9a3136b60123308d25cc049
thiagolopes <thiagolopes@protonmail.com>
2024-10-01 18:27:42 -0300

    Fixing ignore, add custom file

---
+++ custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-by-copying t)
+ '(custom-enabled-themes '(zenburn))
+ '(custom-safe-themes
+   '("09b833239444ac3230f591e35e3c28a4d78f1556b107bafe0eb32b5977204d93" default))
+ '(indicate-buffer-boundaries 'left)
+ '(indicate-empty-lines t)
+ '(package-selected-packages
+   '(virtualenvwrapper dumb-jump pyvenv pyenv-mode orderless mct consult solaire-mode ctrlf dockerfile-mode lua-mode rust-mode yaml-mode undo-fu undo-fu-session vundo rainbow-delimiters org-modern magit zenburn-theme))
+ '(require-final-newline t)
+ '(undo-limit 67108864)
+ '(undo-strong-limit 100663296)
+ '(use-dialog-box nil)
+ '(user-mail-address "thiagolopes@protonmail.com")
+ '(x-underline-at-descent-line t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 180 :family "IosevkaNerdFont")))))
