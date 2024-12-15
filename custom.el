ead29add250ceb2e82a658c16617b166dbd5f056
thiagolopes <thiagolopes@protonmail.com>
2024-12-14 21:11:22 -0300

    Iosevka -> AnonymousPro

--- custom.el
+++ custom.el
@@ -49,4 +49,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 180 :family "IosevkaNerdFont")))))
+ '(default ((t (:height 180 :family "AnonymiceProNerdFont")))))
