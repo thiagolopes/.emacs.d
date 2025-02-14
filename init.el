fec3fb2c919da4ac16943097f87943b60eca3f61
thiagolopes <thiagolopes@protonmail.com>
2025-02-14 17:07:39 -0300

    package: bm https://github.com/joodland/bm

--- init.el
+++ init.el
@@ -158,3 +158,11 @@
 (use-package marginalia
   :init
   (marginalia-mode 1))
+(use-package bm
+  :init
+  (setq bm-restore-repository-on-load t)
+  :config
+  (setq bm-cycle-all-buffers t)
+  :bind (("<f2>" . bm-next)
+         ("S-<f2>" . bm-previous)
+         ("C-<f2>" . bm-toggle)))
