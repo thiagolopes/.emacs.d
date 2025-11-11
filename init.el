d2628e24c0dde8354b952d90c2f7253f5140f88e
thiagolopes <thiagolopes@protonmail.com>
2025-11-10 23:12:47 -0300

    fix(tree-sitter): remove modes

--- init.el
+++ init.el
@@ -387,8 +387,6 @@
   (org-mode . visual-fill-column-mode))
 
 (use-package treesit-auto
-  :mode (("\\.ts\\'" . typescript-ts-mode)
-         ("\\.tsx\\'" . tsx-ts-mode))
   :custom
   (treesit-auto-install t)
   :config
