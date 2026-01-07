0afb8ae14e6b67ade590b3cdf008fe32cf0e181b
thiagolopes <thiagolopes@protonmail.com>
2026-01-07 11:18:42 -0300

    feat(theme): small font

--- custom.el
+++ custom.el
@@ -117,7 +117,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 160 :family "Iosevka" :weight light))))
+ '(default ((t (:height 140 :family "Iosevka"))))
  '(modus-themes-ui-variable-pitch ((t (:inherit variable-pitch))) t)
  '(region ((t :extend nil)))
  '(variable-pitch ((t (:family "Roboto")))))
