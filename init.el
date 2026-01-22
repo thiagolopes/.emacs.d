d34405004093904bc8a895b1bac0c0b95796eecd
thiagolopes <thiagolopes@protonmail.com>
2026-01-22 17:36:41 -0300

    feat: org better config

--- init.el
+++ init.el
@@ -275,7 +275,15 @@
   (org-mode . auto-fill-mode)
   (org-mode . verb-mode)
   :custom
+  (org-auto-align-tags nil)
+  (org-tags-column 0)
+  (org-catch-invisible-edits 'show-and-error)
+  (org-special-ctrl-a/e t)
+  (org-insert-heading-respect-content t)
   (org-hide-emphasis-markers t)
+  (org-pretty-entities t)
+  (org-agenda-tags-column 0)
+  (org-ellipsis "…")
   (org-modern-fold-stars
    '(("▶" . "▼")
      ("▷▷" . "▽▽")
