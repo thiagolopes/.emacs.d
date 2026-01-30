44b8e11e407810379931bb8425b98a8b1d614e6c
thiagolopes <thiagolopes@protonmail.com>
2026-01-30 11:29:44 -0300

    feat: remove deprecated consult config

--- init.el
+++ init.el
@@ -302,8 +302,6 @@
    consult-theme :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep consult-man
    consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
    ;; :preview-key "M-."
    :preview-key '(:debounce 0.8 any))
   ;; :custom
